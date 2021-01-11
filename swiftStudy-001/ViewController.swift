1d9b37a5e0b64070c9ef531dec3543697129873b
Shukou Nakazawa <shukou.nakazawa@gmail.com>
2021-01-11 17:54:07 +0900

    Initial Commit

---
+++ swiftStudy-001/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  swiftStudy-001
+//
+//  Created by Shukou Nakazawa on 2021/01/11.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ swiftStudy-001Tests/swiftStudy_001Tests.swift
@@ -0,0 +1,33 @@
+//
+//  swiftStudy_001Tests.swift
+//  swiftStudy-001Tests
+//
+//  Created by Shukou Nakazawa on 2021/01/11.
+//
+
+import XCTest
+@testable import swiftStudy_001
+
+class swiftStudy_001Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
